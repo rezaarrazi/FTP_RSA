0053f08edcdee500bb29c9db4b374195dfa6b6d5
rezaarrazi <rezaarrazi@gmail.com>
2018-05-30 08:27:14 +0700

    Add files via upload

--- ftp/client/FTPClient.c
+++ ftp/client/FTPClient.c
@@ -10,27 +10,37 @@
 #include<math.h>
 #include<string.h>
  
-long int p,q,pub_n,t,flag,e[100],d[100],temp[100],j,m[100],i;
+#include <dirent.h>
+
+long int p,q,n,pub_n,pub_e,t,flag,e[100],d[100],temp[100],j,m[100],i,len;
 int en[100];
+char en_msg[512];
 char de_msg[100];
+char send_msg[512];
 int _index;
 int private_d;
+long int buffsize;
+
+long int public_e;
+long int public_n;
 
 int _j;
 
+void encrypt(long int pKey, long int nKey);
 void generateKey();
 int prime(long int);
 void ce();
-long int cd(long int); 
-void encrypt();
+long int cd(long int);
 void decrypt();
 
 int main()
 {
     FILE *fp;
     int csd,n,ser,s,cli,cport,newsd;
-    char name[100],rcvmsg[100],rcvg[512],fname[100];
+    char name[100],rcvmsg[100],rcvg[512],fname[100],fileread[100];
     struct sockaddr_in servaddr;
+    char buffer[1025];
+
     printf("Masukkan port\t: ");
     scanf("%d",&cport);
     csd=socket(AF_INET,SOCK_STREAM,0);
@@ -39,6 +49,7 @@ int main()
         printf("Error....\n");
         exit(0);
     }
+    bool rsa = true;
 
     memset(&servaddr, '0', sizeof(servaddr));
 
@@ -46,86 +57,278 @@ int main()
     servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
     servaddr.sin_port=htons(cport);
     // Convert IPv4 and IPv6 addresses from text to binary form
-	if(inet_pton(AF_INET, "192.168.200.68", &servaddr.sin_addr)<=0) 
+	if(inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr)<=0) 
 	{
 		printf("\nInvalid address/ Address not supported \n");
 		return -1;
 	}
-    
-    if(connect(csd,(struct sockaddr *)&servaddr,sizeof(servaddr))<0){
-        printf("Error in connection\n");
-        exit(0);
-    }
-    else
-        printf("connected\n");
-
-    bool rsa = false;
 
     if(rsa)
         generateKey();
 
-    printf("Nama file yang akan didownload\t: ");
-    scanf("%s",name);
-    printf("Simpan file dengan nama\t\t: ");
-    scanf("%s",fname);
-    fp=fopen(fname,"w");
+    int mode = 1;
 
-    // printf("buffer: %s\n", buffer);
-    if(rsa){
-        char buffer[100];
-        sprintf(buffer, "%s:%ld:%ld", name, e[_index-1], pub_n);
-        send(csd,buffer,sizeof(buffer),0);
+    if(connect(csd,(struct sockaddr *)&servaddr,sizeof(servaddr))<0){
+        printf("Error in connection\n");
+        exit(0);
     }
     else{
-        send(csd,name,sizeof(name),0);
+        _index = 0;
+        if(rsa){
+            s=recv(csd,rcvg,512,0);
+            // rcvg[s]='\0';
+            strcpy(buffer, rcvg);
+            // printf("#pesan: %s\n", buffer);
+            char * segmen;
+            segmen = strtok (buffer,":");
+            // printf("#segmen: %s\n", segmen);
+            while (segmen != NULL)
+            {
+                if (_index == 0)
+                {
+                    public_e = atoi(segmen);
+                    printf("server e: %ld\n", public_e );
+                    _index++;
+                }
+                else if (_index == 1)
+                {
+                    public_n = atoi(segmen);
+                    printf("server n: %ld\n", public_n );
+                    _index = 0;
+                }
+                segmen = strtok (NULL,":");
+            }
+        }
+
+        printf("connected\n");
     }
-        
-    while(1)
-    {
-        s=recv(csd,rcvg,512,0);
-        rcvg[s]='\0';
-        if(strcmp(rcvg,"error")==0)
-            printf("File yang anda download tidak ada\n");
-        if(strcmp(rcvg,"completed")==0)
-        {
-            printf("\nBerhasil mendownload file!\n");
-            fclose(fp);
-            close(csd);
-            break;
+
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("/home/rangga/Documents/FTP_RSA-master/ftp/server");
+    if (d) {
+        printf("\nFile Di Server\n");
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    }
+
+    system("clear");
+    printf("1. Download\n");
+    printf("2. Upload\n");
+    printf("Option : ");
+    scanf("%d", &mode);
+
+    if(mode==1){
+        send(csd,"download",8,0);
+        usleep(1000);
+
+        printf("Nama file yang akan didownload\t: ");
+        scanf("%s",name);
+        printf("Simpan file dengan nama\t\t: ");
+        scanf("%s",fname);
+        fp=fopen(fname,"w");
+
+        // printf("buffer: %s\n", buffer);
+        if(rsa){
+            char sendbuffer[100];
+            sprintf(sendbuffer, "%s:%ld:%ld", name, pub_e, pub_n);
+            printf("..>%s\n", sendbuffer);
+            send(csd,sendbuffer,sizeof(sendbuffer),0);
         }
         else{
-            if(s>0){
-                if(rsa){
-                    printf("size: %d\n", s);
-                    int ex;
-                    // for(ex=s-1; ex>=0; ex--){
-                    //     printf("%d.", rcvg[ex]);
-                    // }
-                    long int nChar = s/4;
+            send(csd,name,sizeof(name),0);
+        }
+        usleep(500);
 
-                    for(ex=0; ex<nChar; ex++){
-                        long int a = 0;
-                        int jInd;
-                        // printf("\n======================\n");
-                        // printf("->a now : %ld\n", a);
-                        for( jInd = 3; jInd>=0; jInd--){
-                            a = a << 7;
-                            a = a | rcvg[(4*ex) + jInd];
-                            // printf("data[%d]:%d| ", (4*ex) + jInd, rcvg[(4*ex) + jInd]);
+        while(1)
+        {
+            s=recv(csd,rcvg,512,0);
+            rcvg[s]='\0';
+            printf("----> pesan: %s\n", rcvg);
+            if(strcmp(rcvg,"error")==0){
+                printf("File yang anda download tidak ada\n");
+                send(csd,"bye",3,0);
+
+                fclose(fp);
+                close(csd);
+                break;
+            }
+                
+            if(strcmp(rcvg,"completed")==0)
+            {
+                printf("\nBerhasil mendownload file!\n");
+                
+                send(csd,"bye",3,0);
+
+                fclose(fp);
+                close(csd);
+                break;
+            }
+            
+            if(strcmp(rcvg,"Welcome")==0)
+            {
+                printf("Welcome message recieved!\n");
+            }
+            else{
+                if(s>0){
+                    if(rsa){
+                        printf("size: %d\n", s);
+                        int ex;
+                        // for(ex=s-1; ex>=0; ex--){
+                        //     printf("%d.", rcvg[ex]);
+                        // }
+                        long int nChar = s/4;
+
+                        for(ex=0; ex<nChar; ex++){
+                            long int a = 0;
+                            int jInd;
+                            // printf("\n======================\n");
+                            // printf("->a now : %ld\n", a);
+                            for( jInd = 3; jInd>=0; jInd--){
+                                a = a << 7;
+                                a = a | rcvg[(4*ex) + jInd];
+                                // printf("data[%d]:%d| ", (4*ex) + jInd, rcvg[(4*ex) + jInd]);
+                            }
+                            // printf("\na now : %ld\n", a);
+                            en[ex] = a;
                         }
-                        // printf("\na now : %ld\n", a);
-                        en[ex] = a;
+                        en[nChar] = -1;
+                        decrypt();
+
+                        fprintf(fp,"%s",de_msg);
                     }
-                    en[nChar] = -1;
-                    decrypt();
-                }
-                else{
-                    fputs(rcvg,stdout);
+                    else{
+                        printf("langsung | size: %d\n", s);;
+                        printf("Pesan: %s\n", rcvg);
+                        fprintf(fp,"%s",rcvg);
+                    }
                 }
             }
         }
-        if(rsa) fprintf(fp,"%s",de_msg);
     }
+    else{
+        send(csd,"upload",6,0);
+        usleep(1000);
+
+        printf("Nama file yang akan diupload\t: ");
+        scanf("%s",name);
+        fp=fopen(name,"r");
+        if(fp==NULL)
+        {
+            send(csd,"error",5,0);
+            close(csd);
+        }
+        else
+        {
+            send(csd,name,strlen(name),0);
+            usleep(2000);
+
+            while(fgets(fileread,sizeof(fileread),fp))
+            {
+                printf("Pesannya : %s\n", fileread);
+                len = strlen(fileread);
+                
+                if(rsa){
+                    int ind=0;
+                    for (ind=0;fileread[ind]!=NULL;ind++){
+                        m[ind]= fileread[ind];
+                        // printf("%c %ld, ", fileread[ind], m[ind]);
+                    }
+                    encrypt(public_e, public_n);
+                    
+                    // sprintf(send_msg, "%s|%s", name, en_msg);
+                    printf("dikirim : %s, buffsize: %d\n", en_msg, buffsize);
+                    // buffsize += strlen(name)+1;
+                    if(send(csd, en_msg, buffsize,0)<0)
+                    {
+                        printf("Cant send\n");
+                    }
+                }
+                else{
+                    int _len = strlen(fileread);
+                    printf("Pesan : %s, panjang: %d\n", fileread, _len);
+                    _len += strlen(name)+1;
+                    sprintf(send_msg, "%s|%s", name, fileread);
+                    if(send(csd, send_msg, _len,0)<0)
+                    {
+                        printf("Cant send\n");
+                    }
+
+                    usleep(500);
+                }
+                
+                usleep(500);
+            }
+
+            printf("Sent\n");
+            send(csd,"bye",3,0);
+
+            fclose(fp);
+            close(csd);
+        }
+    }
+    
+        
+    
+}
+
+void encrypt(long int pKey, long int nKey) {
+    long int pt,ct,key=pKey,k;
+    int i = 0;
+    n = nKey;
+	
+    printf("len: %ld, key: %ld\n", len, key);
+    while(i!=len) {
+        pt=m[i];
+        k=1;
+ 
+		for (j=0;j<key;j++) {
+            k=k*pt;
+            k=k%n;
+		}
+
+		en[i]=k;
+		// printf("(%ld:%c:%ld), ", i, (char)m[i], en[i]);
+        i++;
+	}
+    // printf("\n");
+ 
+	en[i]=-1;
+
+    int _j=0;
+    for (i=0;en[i]!=-1;i++){
+	    long int aq = en[i];
+        // char b[500];
+        
+        // if(aq >=0 ){
+            // printf("aq : %ld\n", aq);
+            int ex=0;
+            for(ex=0; ex<4; ex++){
+                int tmp = (int) 127 & aq;
+                aq = aq >> 7;
+
+                en_msg[_j] = tmp;
+
+                // printf("\t\tb%d : %d, tmp: %d\n", _j, en_msg[_j], tmp);
+                // printf("\t\ta now : %ld\n", aq);
+                _j++;
+            }
+        // }
+    }
+
+    buffsize = _j;
+    printf("Buffer size: %ld\n", buffsize);
+    // int nj;
+    // printf("======================\n");
+    // for(nj=_j-1; nj>=0; nj--){
+    //     printf("%d.", en_msg[nj]);
+    //     // printf("a now : %ld\n", a);
+    // }
+    // printf("\n");
+    // sprintf(en_msg, "%s", _j, en_msg);
+    // printf("message: %s\n", en_msg);
 }
 
 void generateKey(){
@@ -166,12 +369,10 @@ void generateKey(){
         return;
     }
 
+    pub_e = e[_index-1];
     private_d = d[_index-1];
-    printf("Public key: (%ld, %ld)\n", e[_index-1], pub_n);
-    printf("Private key: (%ld, %ld)\n", d[_index-1], pub_n);
-
-	// encrypt();
-	// decrypt();
+    printf("Public key: (%ld, %ld)\n", pub_e, pub_n);
+    printf("Private key: (%d, %ld)\n", private_d, pub_n);
 }
 
 int prime(long int pr) {
@@ -220,7 +421,7 @@ long int cd(long int x) {
 void decrypt() {
 	long int pt,ct,key=private_d,k;
 	i=0;
-    // printf("\nkey: %ld, n: %ld\n", key, n);
+    printf("\nkey: %ld, n: %ld\n", key, pub_n);
     // printf("\ndecrypt\n");
 	while(en[i]!=-1) {
         ct = en[i];
--- ftp/server/FTPServer.c
+++ ftp/server/FTPServer.c
@@ -1,3 +1,16 @@
+//Example code: A simple server side code, which echos back the received message.
+//Handle multiple socket connections with select and fd_set on Linux 
+#include <stdio.h> 
+#include <string.h> //strlen 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <unistd.h> //close 
+#include <arpa/inet.h> //close 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
+
 #include<stdio.h>
 #include<sys/types.h>
 #include<netinet/in.h>
@@ -11,148 +24,394 @@
 #include<math.h>
 #include<string.h>
  
-long int p,q,n,t,flag,e[100],d[100],temp[100],j,m[100],en[100],i,len;
+long int p,q,n,t,flag,e[100],d[100],temp[100],j,m[100],en[100],len,pub_n,pub_e;
 char en_msg[512];
+char in_buff[512];
 int _index;
-long int public_e;
-long int public_n;
+long int public_e[30];
+long int public_n[30];
 long int buffsize;
 
-void encrypt();
+int mode[30];
 
-int main()
-{
-    FILE *fp;
-    int sd,newsd,ser,n,a,cli,pid,bd,port,clilen;
-    char name[100],fileread[100],fname[100],ch,file[100],rcv[100];
-    struct sockaddr_in servaddr,cliaddr;
-    bool rsa = false;
-    int opt = 1;
+char de_msg[100];
+int private_d;
+int _j;
 
-    printf("Masukkan port\t: ");
-    scanf("%d",&port);
-    sd=socket(AF_INET,SOCK_STREAM,0);
-    if(sd<0)
-        printf("Gagal membuat socket\n");
-    else
-        printf("Berhasil membuat socket pada %d\n", port);
-    
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+	
+#define TRUE 1 
+#define FALSE 0 
+#define PORT 2222 
 
-    cliaddr.sin_family=AF_INET;
-    cliaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-    cliaddr.sin_port=htons(port);
-    a=sizeof(cliaddr);
-    bd=bind(sd,(struct sockaddr *)&cliaddr,a);
+void encrypt(long int pKey, long int nKey);
+void generateKey();
+int prime(long int);
+void ce();
+long int cd(long int); 
+void decrypt();
+	
+int main(int argc , char *argv[]) 
+{ 
+	int opt = TRUE; 
+	int master_socket , addrlen , new_socket , client_socket[30] , 
+		max_clients = 30 , activity, i , valread , sd; 
+	int max_sd; 
+	
+	FILE *fp;
+	int newsd,ser,n,a,cli,pid,bd,port,clilen;
+	char name[100],fileread[100],fname[100],ch,file[100],rcv[100];
+	struct sockaddr_in servaddr,cliaddr;
+	
+	bool rsa = true;
 
-    if(bd<0)
-        printf("Cant bind\n");
-    else
-        printf("Binded\n");
-    
-    listen(sd,5);
-    clilen=sizeof(cliaddr);
-    newsd=accept(sd,(struct sockaddr *)&cliaddr,&clilen);
-    if(newsd<0)
-    {
-        printf("Cant accept\n");
-    }
-    else
-        printf("Accepted\n");
+	struct sockaddr_in address; 
+		
+	char buffer[1025]; //data buffer of 1K 
+		
+	//set of socket descriptors 
+	fd_set readfds; 
+		
+	//a message 
+	char *message = "Welcome"; 
 
-    n=recv(newsd,rcv,150,0);
-    rcv[n]='\0';
-    
-    if(rsa){
-        char filename[100];
+	if(rsa)
+        generateKey();
+	
+	//initialise all client_socket[] to 0 so not checked 
+	for (i = 0; i < max_clients; i++) 
+	{ 
+		client_socket[i] = 0; 
+	} 
+		
+	//create a master socket 
+	if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	//set master socket to allow multiple connections , 
+	//this is just a good habit, it will work without this 
+	if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, 
+		sizeof(opt)) < 0 ) 
+	{ 
+		perror("setsockopt"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	//type of socket created 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	address.sin_port = htons( PORT ); 
+		
+	//bind the socket to localhost port 8888 
+	if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	printf("Listener on port %d \n", PORT); 
+		
+	//try to specify maximum of 3 pending connections for the master socket 
+	if (listen(master_socket, 3) < 0) 
+	{ 
+		perror("listen"); 
+		exit(EXIT_FAILURE); 
+	} 
+		
+	//accept the incoming connection 
+	addrlen = sizeof(address); 
+	puts("Waiting for connections ..."); 
+		
+	while(TRUE) 
+	{ 
+		//clear the socket set 
+		FD_ZERO(&readfds); 
+	
+		//add master socket to set 
+		FD_SET(master_socket, &readfds); 
+		max_sd = master_socket; 
+			
+		//add child sockets to set 
+		for ( i = 0 ; i < max_clients ; i++) 
+		{ 
+			//socket descriptor 
+			sd = client_socket[i]; 
+				
+			//if valid socket descriptor then add to read list 
+			if(sd > 0) 
+				FD_SET( sd , &readfds); 
+				
+			//highest file descriptor number, need it for the select function 
+			if(sd > max_sd) 
+				max_sd = sd; 
+		} 
+	
+		//wait for an activity on one of the sockets , timeout is NULL , 
+		//so wait indefinitely 
+		activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL); 
+	
+		if ((activity < 0) && (errno!=EINTR)) 
+		{ 
+			printf("select error"); 
+		} 
+			
+		//If something happened on the master socket , 
+		//then its an incoming connection 
+		if (FD_ISSET(master_socket, &readfds)) 
+		{ 
+			if ((new_socket = accept(master_socket, 
+					(struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
+			{ 
+				perror("accept"); 
+				exit(EXIT_FAILURE); 
+			} 
+			
+			//inform user of socket number - used in send and receive commands 
+			printf("New connection , socket fd is %d , ip is : %s , port : %d\n" , new_socket , inet_ntoa(address.sin_addr) , ntohs
+				(address.sin_port)); 
+		
+			if(rsa){
+				char buffer[100];
+				sprintf(buffer, "%ld:%ld", pub_e, pub_n);
+				send(new_socket, buffer,sizeof(buffer),0);
+			}
+			// //send new connection greeting message 
+			// if( send(new_socket, message, strlen(message), 0) != strlen(message) ) 
+			// { 
+			// 	perror("send"); 
+			// }
+			usleep(500);
+				
+			// puts("Welcome message sent successfully"); 
+				
+			//add new socket to array of sockets 
+			for (i = 0; i < max_clients; i++) 
+			{ 
+				//if position is empty 
+				if( client_socket[i] == 0 ) 
+				{ 
+					client_socket[i] = new_socket; 
+					printf("Adding to list of sockets as %d\n" , i); 
+						
+					break; 
+				} 
+			} 
+		} 
+			
+		for (i = 0; i < max_clients; i++) 
+		{ 
+			sd = client_socket[i]; 
+				
+			if (FD_ISSET( sd , &readfds)) 
+			{ 
+                valread = read( sd , buffer, 1024);
+                buffer[valread] = '\0';
+				// printf("buffer: %s, valread: %d\n", buffer, valread);
+				
+				if( strcmp("download", buffer) == 0) 
+				{
+					// printf("down\n");
+					mode[i] = 1;
+					break;
+				}
+				else if( strcmp("upload", buffer) == 0) 
+				{
+					// printf("up\n");
+					mode[i] = 2;
+					break;
+				}
 
-        char * segmen;
-        segmen = strtok (rcv,":");
-        while (segmen != NULL)
-        {
-            if (_index == 0) 
-            {
-                printf("filename: %s\n", segmen );
-                strcpy(filename, segmen);
-                _index++;
-            }
-            else if (_index == 1)
-            {
-                public_e = atoi(segmen);
-                printf("e: %ld\n", public_e );
-                _index++;
-            }
-            else if (_index == 2)
-            {
-                public_n = atoi(segmen);
-                printf("n: %ld\n", public_n );
-                _index++;
-            }
-            segmen = strtok (NULL,":");
-        }
+				if(mode[i] == 1){
+					if ( strcmp("bye", buffer) == 0) 
+					{ 
+						//Somebody disconnected , get his details and print 
+						getpeername(sd , (struct sockaddr*)&address , \
+							(socklen_t*)&addrlen); 
+						printf("Host disconnected , ip %s , port %d \n" , 
+							inet_ntoa(address.sin_addr) , ntohs(address.sin_port)); 
+							
+						//Close the socket and mark as 0 in list for reuse 
+						close( sd ); 
+						client_socket[i] = 0; 
+						public_e[i] = 0;
+						public_n[i] = 0;
+					}
+					else
+					{ 
+						//set the string terminating NULL byte on the end 
+						//of the data read 
+						
+						if(rsa){
+							char filename[100];
+							_index = 0;
+							strcpy(rcv, buffer);
+							char * segmen;
+							segmen = strtok (rcv,":");
+							while (segmen != NULL)
+							{
+								if (_index == 0) 
+								{
+									printf("filename: %s\n", segmen );
+									strcpy(filename, segmen);
+									_index++;
+								}
+								else if (_index == 1)
+								{
+									public_e[i] = atoi(segmen);
+									printf("e: %ld\n", public_e[i] );
+									_index++;
+								}
+								else if (_index == 2)
+								{
+									public_n[i] = atoi(segmen);
+									printf("n: %ld\n", public_n[i] );
+									_index = 0;
+								}
+								segmen = strtok (NULL,":");
+							}
 
-        fp=fopen(filename,"r");
-    }
-    else{
-        fp=fopen(rcv,"r");
-    }
+							fp=fopen(filename,"r");
 
-    if(fp==NULL)
-    {
-        send(newsd,"error",5,0);
-        close(newsd);
-    }
-    else
-    {
-        while(fgets(fileread,sizeof(fileread),fp))
-        {
-            printf("Pesan : %s\n", fileread);
-            len = strlen(fileread);
-            
-            if(rsa){
-                int ind=0;
-                for (ind=0;fileread[ind]!=NULL;ind++){
-                    m[ind]= fileread[ind];
-                    // printf("%c %ld, ", fileread[ind], m[ind]);
-                }
-                // for (ind=0;m[ind]!=NULL;ind++){
-                //     printf("%ld, ", m[ind]);
-                // }
-                // printf("\n");
-                encrypt();
+							if(fp==NULL)
+							{
+								send(sd,"error",5,0);
+								close(sd);
+							}
+							else
+							{
+								while(fgets(fileread,sizeof(fileread),fp))
+								{
+									printf("RSA Pesan : %s\n", fileread);
+									len = strlen(fileread);
+									
+									if(rsa){
+										int ind=0;
+										for (ind=0;fileread[ind]!=NULL;ind++){
+											m[ind]= fileread[ind];
+											// printf("%c %ld, ", fileread[ind], m[ind]);
+										}
+										encrypt(public_e[i], public_n[i]);
 
-                if(send(newsd,en_msg,buffsize,0)<0)
-                {
-                    printf("Cant send\n");
-                }
-            }
-            else{
-                if(send(newsd,fileread,len,0)<0)
-                {
-                    printf("Cant send\n");
-                }
-            }
-            
-            sleep(1);
-        }
-        // if(!fgets(fileread,sizeof(fileread),fp))
-        // {
-            send(newsd,"completed",999999999,0);
-        // }
-        return(0);
-    }
-}
+										if(send(sd,en_msg,buffsize,0)<0)
+										{
+											printf("Cant send\n");
+										}
+									}
+									
+									usleep(500);
+								}
 
-void encrypt() {
-    long int pt,ct,key=public_e,k;
-    i = 0;
-    n = public_n;
+								printf("Sent\n");
+								if(send(sd,"completed",9,0) < 0) printf("not done\n");
+								else printf("Done\n");
+							}
+
+							fclose(fp);
+						}
+						else{
+							fp=fopen(buffer,"r");
+
+							if(fp==NULL)
+							{
+								send(sd,"error",5,0);
+								close(sd);
+							}
+							else
+							{
+								while(fgets(fileread,sizeof(fileread),fp))
+								{
+									len = strlen(fileread);
+									printf("Pesan : %s, panjang: %ld\n", fileread, len);
+									if(send(sd,fileread,len,0)<0)
+									{
+										printf("Cant send\n");
+									}
+
+									usleep(500);
+								}
+
+								printf("Sent\n");
+								if(send(sd,"completed",9,0) < 0) printf("not done\n");
+								else printf("Done\n");
+							}
+
+							fclose(fp);
+						}
+						
+						//send(sd , buffer , strlen(buffer) , 0 ); 
+					}
+				}
+				else if(mode[i] == 2){
+					if ( strcmp("bye", buffer) == 0) 
+					{ 
+						//Somebody disconnected , get his details and print 
+						getpeername(sd , (struct sockaddr*)&address , \
+							(socklen_t*)&addrlen); 
+						printf("Host disconnected , ip %s , port %d \n" , 
+							inet_ntoa(address.sin_addr) , ntohs(address.sin_port)); 
+							
+						//Close the socket and mark as 0 in list for reuse 
+						close( sd );
+						fclose(fp);
+						
+						client_socket[i] = 0; 
+						public_e[i] = 0;
+						public_n[i] = 0;
+					}
+					else{
+						if(buffer[valread-1] == 't' &&  buffer[valread-2] == 'x' && buffer[valread-3] == 't'){
+							printf("filename: %s\n", buffer );
+							char filename[100];
+							strcpy(filename, buffer);
+
+							fp=fopen(filename,"w");
+						}
+						else{
+							if(rsa){
+								printf("size: %d\n", valread);
+								int ex;
+								// for(ex=valread-1; ex>=0; ex--){
+								//     printf("%d.", buffer[ex]);
+								// }
+								long int nChar = valread/4;
+
+								for(ex=0; ex<nChar; ex++){
+									long int a = 0;
+									int jInd;
+									// printf("\n======================\n");
+									// printf("->a now : %ld\n", a);
+									for( jInd = 3; jInd>=0; jInd--){
+										a = a << 7;
+										a = a | buffer[(4*ex) + jInd];
+										// printf("data[%d]:%d| ", (4*ex) + jInd, rcvg[(4*ex) + jInd]);
+									}
+									// printf("\na now : %ld\n", a);
+									en[ex] = a;
+								}
+								en[nChar] = -1;
+								decrypt();
+
+								fprintf(fp,"%s",de_msg);
+							}
+							else{
+								fprintf(fp,"%s",buffer);
+								_index=0;
+							}
+						}
+					}
+				}
+			} 
+		} 
+	} 
+		
+	return 0; 
+} 
+
+void encrypt(long int pKey, long int nKey) {
+    long int pt,ct,key=pKey,k;
+    int i = 0;
+    n = nKey;
 	
     printf("len: %ld, key: %ld\n", len, key);
     while(i!=len) {
@@ -204,4 +463,124 @@ void encrypt() {
     // printf("\n");
     // sprintf(en_msg, "%s", _j, en_msg);
     // printf("message: %s\n", en_msg);
+}
+
+void generateKey(){
+    printf("Masukkan bilangan prima pertama: ");
+    scanf("%ld",&p);
+
+	flag=prime(p);
+    if(flag==0) {
+    	printf("INPUT BUKAN BILANGAN PRIMA!\n");
+ 
+		exit(1);
+	}
+
+	printf("Masukkan bilangan prima kedua: ");
+	scanf("%ld",&q);
+
+	flag=prime(q);
+	if(flag==0||p==q) {
+        printf("INPUT BUKAN BILANGAN PRIMA!\n");
+ 
+		exit(1);
+	}
+ 
+	pub_n=p*q;
+    t=(p-1)*(q-1);
+    // printf("n: %ld", pub_n);
+
+	ce();
+	int i = 0;
+
+	printf("POSSIBLE VALUES OF e AND d ARE\n");
+	for (i=0;i<j-1;i++)
+        printf("%d) %ld\t%ld\n",i+1,e[i],d[i]);
+
+    printf("Pilih key: ");
+    scanf("%d", &_index);
+    if(_index < 0 || _index >= (int)j){
+        printf("Out of index\n");
+        return;
+    }
+
+    private_d = d[_index-1];
+	pub_e = e[_index-1];
+    printf("Public key: (%ld, %ld)\n", pub_e, pub_n);
+    printf("Private key: (%d, %ld)\n", private_d, pub_n);
+}
+
+int prime(long int pr) {
+    int i;
+ 
+	j=sqrt(pr);
+    for (i=2;i<=j;i++) {
+    	if(pr%i==0)
+            return 0;
+	}
+ 
+	return 1;
+}
+ 
+void ce() {
+    int k = 0;
+	int i = 0;
+	for (i=2;i<t;i++) {
+        if(t%i==0)
+            continue;
+ 
+		flag=prime(i);
+        if(flag==1&&i!=p&&i!=q) {
+            e[k]=i;
+            flag=cd(e[k]);
+ 
+			if(flag>0) {
+                d[k]=flag;
+                k++;
+            }
+ 
+			if(k==99)
+                break;
+        }
+	}
+}
+ 
+long int cd(long int x) {
+	long int k=1;
+	while(1) {
+        k=k+t;
+        if(k%x==0)
+            return(k/x);
+	}
+}
+ 
+void decrypt() {
+	long int pt,ct,key=private_d,k;
+	int i = 0;
+    // printf("\nkey: %ld, n: %ld\n", key, n);
+    // printf("\ndecrypt\n");
+	while(en[i]!=-1) {
+        ct = en[i];
+        
+        k=1;
+ 
+		for (j=0;j<key;j++) {
+            k=k*ct;
+            k=k%pub_n;
+		}
+ 
+		// printf("%ld", pt);
+        m[i]=k;
+        de_msg[i] = k;
+		i++;
+	}
+    // printf("\n");
+	
+    m[i]=-1;
+    printf("\nTHE DECRYPTED MESSAGE IS\n");
+    printf("%s\n",de_msg);
+	// for (i=0;m[i]!=-1;i++){
+    //     printf("%c",(char) m[i]);
+    // }
+    printf("\n");
 }
